2466cbce1e7615308ac7d97f5d0222ab272ce515
jobemcamera <jobecamera@gmail.com>
2022-11-08 18:53:07 -0300

    Adicionado feedback de sucesso ao confirmar o cadastro. Fixed #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,42 @@ var password = document.getElementById("password")
 var confirmPassword = document.getElementById("confirmpassword")
 var confirmForm = document.getElementById("confirmform")
 var wrongPassword = document.getElementById("wrongpassword")
+var fullName = document.getElementById("fullname")
+var birth = document.getElementById("birth")
+var email = document.getElementById("email")
+var number = document.getElementById("number")
 
+confirmForm.addEventListener("click", sucessForm)
+
+/* Função que verifica se há conteúdos nos campos de input. Confirma os dados ao usuário*/
+function sucessForm() {
+    if (fullName.value.length == 0 || birth.value.length == 0 || email.value.length == 0 || number.value.length == 0 || password.value.length == 0 || confirmPassword.value.length == 0 ) {
+        alert ("Você deve preencher todos os campos!")
+    } else {
+        alert ("Dados confirmados com SUCESSO!")
+        fullName.value = ""
+        birth.value = ""
+        email.value = ""
+        number.value = ""
+        password.value = ""
+        confirmPassword.value = ""
+    }
+} 
+
+/* Função para que cria a máscara do telefone (xx)xxxxx-xxxx */
+function phoneMask(number) {
+    if (number.value.length == 0) {
+        number.value = '(' + number.value;
+    } 
+    if (number.value.length == 3) {
+        number.value = number.value + ')'
+    }
+    if (number.value.length == 9) {
+        number.value = number.value + '-'
+    }
+}
+
+/* Função que verifica se as senhas são iguais */
 function checkPassword() {
     if (password.value != confirmPassword.value) {
         wrongPassword.style.display ="flex"
@@ -17,3 +52,4 @@ password.addEventListener("keyup", () => {
 })
 
 confirmPassword.addEventListener("keyup", checkPassword)
+
